d8185882ab1aac80aeef924eb34e09bd31c639ff
Brunno dos Santos <squiter85@gmail.com>
2015-09-09 11:19:11 -0300

    First exercise

---
+++ 4_exercises.hs
@@ -0,0 +1,17 @@
+-- 01 I don't understand what I need to do :(
+
+-- 02
+n = a `div` length xs
+  where
+    a = 10
+    xs = [1,2,3,4,5]
+
+-- 03
+e3 = head ( reverse [1,2,3] )
+
+-- 04
+e4 = head (drop 2 [1,2,3])
+
+-- 05
+e51 = reverse( drop 1 (reverse [1,2,3]))
+e52 = reverse( tail (reverse [1,2,3]))
