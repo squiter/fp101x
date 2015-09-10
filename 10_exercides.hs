ad2b1468882a8da7b2e579f1cad8f63d4c0e529b
Brunno dos Santos <squiter85@gmail.com>
2015-09-10 19:59:56 -0300

    Second exercise

---
+++ 10_exercides.hs
@@ -0,0 +1,14 @@
+-- 01
+-- [Char]
+-- (Char,Char,Char)
+-- [(Bool,Char)]
+-- ([Bool],[Char])
+-- [[a] -> [a]] # olha a pegadinha aew
+
+-- 02
+-- [a] -> a
+-- (a,b) -> (b,a)
+-- a -> b -> (a,b)
+-- Num a -> a -> a
+-- Eq a => [a]-> Bool
+-- (a -> a) -> a -> a # Porque?
